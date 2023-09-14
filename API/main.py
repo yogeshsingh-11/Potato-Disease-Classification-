5470b601b4de3febeb064910d97b7f6cfbcfa676
yogeshsingh-11 <116149692+yogeshsingh-11@users.noreply.github.com>
2023-09-14 16:14:01 +0530

    Importing the useful libraries

--- API/main.py
+++ API/main.py
@@ -1,1 +1,28 @@
 
+
+from fastapi import FastAPI, File, UploadFile
+from fastapi.middleware.cors import CORSMiddleware
+import uvicorn
+import numpy as np
+from io import BytesIO
+from PIL import Image
+import tensorflow as tf
+
+app = FastAPI()
+
+origins = [
+    "http://localhost",
+    "http://localhost:3000",
+]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+MODEL = tf.keras.models.load_model("../saved_models/1")
+
+CLASS_NAMES = ["Early Blight", "Late Blight", "Healthy"]
+
